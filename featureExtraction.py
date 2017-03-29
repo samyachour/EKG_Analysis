bbe9a5e5a5e3a12364901ca22d767fb5a943e15c
samy achour <sachour96@gmail.com>
2017-03-29 16:42:57 -0700

    started trying to filter out noisy records

--- featureExtraction.py
+++ featureExtraction.py
@@ -8,14 +8,16 @@ import wave # this is the wave.py file in the local folder
 # Reading in matlab data
 
 mat = scipy.io.loadmat('../Physionet_Challenge/training2017/A00001.mat')
-data = np.divide(mat['val'][0],1000)[2:1002]
+data = np.divide(mat['val'][0],1000)[2:502]
+
+reference = pd.read_csv('../Physionet_Challenge/training2017/REFERENCE.csv', names = ["file", "answer", "SNR"]) # N O A ~
     
 
 # Run Wavelet transforms
 
 wave.plotWave(data, "Original Signal", "Index n * 0.003")
 
-rebuilt = wave.waveletDecomp(data, 'sym5', 5, omissions=([1,5], True))
+rebuilt = wave.waveletDecomp(data, 'sym5', 5, omissions=([1,3,5], True))
 wave.plotWave(rebuilt, "rebuilt", "Index n * 0.003")
 
 
@@ -32,10 +34,18 @@ peaks = np.zeros_like(data)
 
 def getRecords(type):
     
-    reference = pd.read_csv('../Physionet_Challenge/training2017/REFERENCE.csv') # N O A ~
-    subset = reference.ix[df[1]==type]
+    subset = reference.ix[reference['answer']==type]
     return subset
 
-print(getRecords("~"))
+noisy = getRecords("~")
+for i, row in noisy.iterrows():
+    data = scipy.io.loadmat('../Physionet_Challenge/training2017/{0}.mat'.format(row['file']))
+    data = np.divide(data['val'][0],1000)
+    # wave.plotWave(data[2:502], "noisy", "index")
+    SNR_val = scipy.stats.signaltonoise(data)
+    noisy.set_value(i,'SNR',SNR_val)
+
+print(noisy)
+
 print(scipy.stats.signaltonoise(data))
 print(scipy.stats.signaltonoise(rebuilt))
