403906de90b68d3c4d9f4cde3c9ece435c68096b
samy achour <sachour96@gmail.com>
2017-03-29 17:06:18 -0700

    code cleanup

--- featureExtraction.py
+++ featureExtraction.py
@@ -9,17 +9,17 @@ import wave # this is the wave.py file in the local folder
 
 mat = scipy.io.loadmat('../Physionet_Challenge/training2017/A00001.mat')
 data = np.divide(mat['val'][0],1000)
-data = data[:500]
+data = data[:1000]
 
 reference = pd.read_csv('../Physionet_Challenge/training2017/REFERENCE.csv', names = ["file", "answer"]) # N O A ~
     
 
 # Run Wavelet transforms
 
-wave.plotWave(data, "Original Signal", "Index n * 0.003")
+wave.plotWave(data[:200], "Original Signal", "Index n * 0.003")
 
-rebuilt = wave.waveletDecomp(data, 'sym5', 5, omissions=([1,3,5], True))
-wave.plotWave(rebuilt, "rebuilt", "Index n * 0.003")
+rebuilt = wave.waveletDecomp(data, 'sym4', 5, omissions=([1,2,4,5], True))
+wave.plotWave(rebuilt[:200], "rebuilt", "Index n * 0.003")
 
 
 # Imperatively grabbing features
@@ -29,6 +29,7 @@ xMax = np.argmax(rebuilt) # location of max peak
 threshold = data[xMax] * 0.35
 peaks = np.zeros_like(data)
 # TODO: Find all the peak intervals using the threshold ad set them into peaks
+# Check out https://www.mathworks.com/examples/wavelet/mw/wavelet-ex77408607-r-wave-detection-in-the-ecg
 
 
 # Detecting noise
@@ -37,3 +38,4 @@ def getRecords(type):
     
     subset = reference.ix[reference['answer']==type]
     return subset
+# TODO: Use fourier transforms to detect noisy datasets
