2fa70a2b6c47f69cd92b49203b992cd4f0934c1f
Work <Work@Dehuas-MacBook-Pro.local>
2017-03-31 13:03:26 -0700

    add functions to calculate residuals

--- wave.py
+++ wave.py
@@ -1,6 +1,7 @@
 import pywt
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io as sio
 
 def plot(y, title, xLab, folder = ""):
     plt.plot(y)
@@ -32,3 +33,42 @@ def decomp(cA, wavelet, levels, mode='constant', omissions=([], False)):
     coeffs = omit(coeffs, omissions)
     
     return pywt.waverecn(coeffs, wavelet, mode=mode)
+
+##helper functions
+def load(filename, path = '../Physionet_Challenge/training2017/'):
+    #
+    ### A helper function to load data
+    # input:
+    #   filename = the name of the .mat file
+    #   path = the path to the file
+    # output:
+    #   data = data output
+    
+    mat = sio.loadmat(path + filename + '.mat')
+    data = np.divide(mat['val'][0],1000)
+    return data
+
+def multiplot(data, graph_names):
+    for l in data:
+        plt.plot(l)
+    plt.legend(graph_names)
+    plt.show()
+    
+def calculate_residuals(original, wavelets, levels, mode='symmetric', omissions=([],True)):
+    rebuilt = decomp(original, wavelets, levels, mode, omissions)
+    residual = sum(abs(original-rebuilt[:len(original)]))/len(original)
+    return residual
+
+def all_residuals(records, wavelets='sym4', levels=3, mode='symmetric', omissions=([1],False), path = '../Physionet_Challenge/training2017/'):
+    residual_list = []
+    file = open(path+records, 'r')
+    while (True):
+        newline = file.readline().rstrip('\n')
+        if newline == '':
+            break
+        data = load(newline)
+        residuals = calculate_residuals(data, wavelets, levels, mode, omissions)
+        residual_list.append(residuals)
+    file.close()
+    return residual_list
+    
