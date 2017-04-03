0078c902036f1b99b9284941155125f335b3edfc
samy achour <sachour96@gmail.com>
2017-04-03 12:19:29 -0700

    added gitignore, renamed file

--- featureExtraction.py
+++ featureExtraction.py
@@ -5,13 +5,13 @@ import wave # this is the wave.py file in the local folder
 # Reading in matlab data
 
 records = wave.getRecords('N')
-mat = wave.load(records[21])
+mat = wave.load(records[9])
 data = mat[:1000]
 
 # Run Wavelet transforms
 
 level = 6
-omission = ([1,2,3], True)
+omission = ([1,2], True)
 
 #wave.plot(data, "Original Signal", "Index n * 0.003")
 rebuilt = wave.decomp(data, 'sym5', level, omissions=omission)
@@ -24,9 +24,9 @@ rebuilt = wave.decomp(data, 'sym5', level, omissions=omission)
 
 # TODO: work on excluding r peaks that don't give good intervals, detect inversion (abs values?)
 # Play with these params
-peaks = wave.detect_peaks(data, mpd=150, show=True)
+peaks = wave.detect_peaks(rebuilt, data, mpd=50, mph=0.5, show=True, valley=True)
 
 # Detecting noise
 
 
-noise_feat_mat, residuals = wave.noise_feature_extract('RECORDS')
+# noise_feat_mat, residuals = wave.noise_feature_extract('RECORDS')
