60f13664d8a898fe9aeb4399b83b96bfb5563e71
Samy (achou100) <sachour96@gmail.com>
2017-04-10 14:19:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,5 +10,8 @@ set -e
 set -o pipefail
 
 pip install --user PyWavelets-0.5.2.tar.gz
+pip install --user numpy-1.12.1.tar.gz
+pip install --user scipy-0.19.0.tar.gz
+pip install --user pandas-0.19.2.tar.gz
 
 chmod a+x challenge.py
